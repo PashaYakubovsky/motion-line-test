7a000272e0b62fbd3fd22bae2758f442cffaf23c
PashaDev2 <pasha.yakubovsky@lynksen.com>
2024-01-21 19:55:52 +0200

    +fix positions

--- src/App.tsx
+++ src/App.tsx
@@ -304,7 +304,7 @@ function App() {
                                 className="item item01"
                                 strokeWidth="1"
                                 r="20"
-                                cx="450"
+                                cx="442"
                                 cy="250"></circle>
                             <circle
                                 stroke="url(#Gradient1)"
